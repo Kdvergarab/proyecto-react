b7e6f756816cb6a539b078f116df51a12df13cbf
Kdvergarab <kdvergarab@gmail.com>
2022-03-07 00:14:43 +0000

    avance

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,37 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	return (<nav className="navbar navbar-expand-md navbar-dark bg-dark" aria-label="Fourth navbar example">
+		<div className="container-fluid "d-flex="bd-highlight">
+			<a className="navbar-brand" href="#">Expand at md</a>
+			<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
+				<span className="navbar-toggler-icon"></span>
+			</button>
+
+			<div className="collapse navbar-collapse" id="navbarsExample04">
+				<ul className="navbar-nav me-auto mb-2 mb-md-0">
+					<li className="nav-item">
+						<a className="nav-link active" aria-current="page" href="#">Home</a>
+					</li>
+					<li className="nav-item">
+						<a className="nav-link" href="#">Link</a>
+					</li>
+					<li className="nav-item">
+						<a className="nav-link disabled">Disabled</a>
+					</li>
+					<li className="nav-item dropdown">
+						<a className="nav-link dropdown-toggle" href="#" id="dropdown04" data-bs-toggle="dropdown" aria-expanded="false">Dropdown</a>
+						<ul className="dropdown-menu" aria-labelledby="dropdown04">
+							<li><a className="dropdown-item" href="#">Action</a></li>
+							<li><a className="dropdown-item" href="#">Another action</a></li>
+							<li><a className="dropdown-item" href="#">Something else here</a></li>
+						</ul>
+					</li>
+				</ul>
+			</div>
 		</div>
-	);
-};
+	</nav>
+	)
+}
 
 export default Home;
